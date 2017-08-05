f08636fa092839918fcf396ceb0017b38a0892cd
DESKTOP-989A86S\KKH <checknow@naver.com>
2017-08-05 12:08:03 +0900

    no message

---
+++ EventTest/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTest
+{
+    delegate void EventHandlerKKH(string message);
+
+    class MyNotifier
+    {
+        public event EventHandlerKKH SomethingHappened;
+
+        public void DoSomething(int number)
+        {
+            int temp = number % 10;
+
+            if (temp != 0 && temp % 3 == 0)
+            {
+                SomethingHappened(String.Format("{0} : 짝짝", number));
+            }
+        }
+    }
+
+    class Program
+    {
+        static public void MyHandler(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        static void Main(string[] args)
+        {
+            var mn = new MyNotifier();
+            mn.SomethingHappened += new EventHandlerKKH(MyHandler);
+
+            for (int i = 1; i < 30; i++)
+            {
+                mn.DoSomething(i);
+            }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Generics;
+using System.Reflection;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] keys = new string[2];
+            keys[0] = "000001";
+            keys[1] = "000002";
+
+            var infBOP = new Dictionary<string, InforceT>();
+            infBOP = Functions.GetTable<string, InforceT>(keys);
+        }
+    }
+
+    class Functions
+    {
+        public static Dictionary<T, U> GetTable<T, U>(T[] keys)
+        {
+            var table = new Dictionary<T, U>();
+
+            var ins1 = (U)Activator.CreateInstance(typeof(U));
+            table.Add(keys[0], ins1);
+
+            var ins2 = (U)Activator.CreateInstance(typeof(U));
+            MethodInfo methodInfo = typeof(U).GetMethod("Cleansing");
+            methodInfo.Invoke(ins2, null);
+            table.Add(keys[1], ins2);
+
+            return table;
+        }        
+    }
+
+    class InforceT
+    {
+        public InforceT()
+        {
+            contNo = "000001";
+            seg = "VA";
+            gurType = 12;
+            av = 0;
+        }   
+
+        public string contNo;
+        public string seg;
+        public int gurType;
+        public double av;
+
+        public void Cleansing()
+        {
+            contNo = "000002";
+            seg = "VUL";
+            gurType = 1;
+            av = 100;
+        }
+    }
+}
