f88e4f00d98df45cc00c927999c357fdb9b5df0b
DESKTOP-989A86S\KKH <checknow@naver.com>
2017-08-09 23:49:00 +0900

    Socket

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row = 100000;
+            int col = 50;
+
+            double[,] sample = new double[row, col];
+            Random rd = new Random();
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    sample[i, j] = rd.NextDouble();
+                }
+            }
+
+            string path = @"D:\StreamTest.txt";
+
+            //파일저장
+            File.Delete(path);
+            using(Stream stream = File.Open(path, FileMode.Create, FileAccess.Write))
+            {
+                BinaryFormatter bformatter = new BinaryFormatter();
+                bformatter.Serialize(stream, sample);
+            }
+
+            //파일열기
+            double[,] res;
+            using (Stream stream = File.Open(path, FileMode.Open, FileAccess.Read))
+            {
+                BinaryFormatter bformatter = new BinaryFormatter();
+                res = (double[,])bformatter.Deserialize(stream);
+            }                      
+
+            Console.ReadLine();
+        }
+    }
+}
