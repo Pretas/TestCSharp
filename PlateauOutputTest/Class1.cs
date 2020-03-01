681b15d8abdffe763ebacb2a99292e3ef043b007
DESKTOP-3SKP3T7\KKH_GRAM <checknow@naver.com>
2020-03-02 01:56:11 +0900

    플라토 아웃풋 테스트

---
+++ PlateauOutputTest/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlateauOutputTest
+{
+    public interface ICFTable
+    {   
+        ICFTable GetCF(IModel model, Policy p, ScenarioManager sm);
+
+        string TableName { get; }
+        void SetTableName(string tn);
+
+        void AddUp(ICFTable cf);
+    }
+
+    public class CFTable1 : ICFTable
+    {
+        public int Member1;
+        public string Member2;
+        public double Member3;
+
+        private string _tableName = "N/A";
+        public string TableName { get => _tableName; }
+
+        public ICFTable GetCF(IModel model, Policy p, ScenarioManager sm)
+        {
+            CFTable1 cf = new CFTable1();
+
+            if (model.GetType() == typeof(Model1))
+            {
+                Model1 modelFrom = model as Model1;
+                cf.Member1 = modelFrom.member1;
+                cf.Member2 = modelFrom.member2;
+                cf.Member3 = modelFrom.member3;
+            }
+
+            return cf;
+        }
+
+        public void AddUp(ICFTable cf)
+        {
+            if (cf.GetType() == typeof(CFTable1))
+            {
+                CFTable1 modelFrom = cf as CFTable1;
+                Member1 = modelFrom.Member1;
+                Member2 = modelFrom.Member2;
+                Member3 = modelFrom.Member3;
+            }
+        }
+
+        public void SetTableName(string tn)
+        {
+            _tableName = tn;
+        }
+    }
+
+    public class Accumlator
+    {
+        // 베이스가 되는 타입
+        public Type CFTableType { get; private set; }
+        
+        // 그루핑된 데이터 저장소 : Dic<subGroup, ICFTable>
+        Dictionary<string, ICFTable> GroupedData;
+
+        public void Init(ICFTable cfT)
+        {
+            CFTableType = cfT.GetType();
+        }
+
+        public void SetCF(IModel model, Policy p, ScenarioManager sM)
+        {
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PlateauOutputTest/Model.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlateauOutputTest
+{
+    public interface IModel
+    { }
+
+    public class ModelBase : IModel
+    {
+        public Accumlator AccumObj;
+        public Policy P;
+        public ScenarioManager SM;
+
+        public int mNo;
+        public int member1;
+        public string member2;
+        public double member3;
+
+        public void Run(Accumlator accumlator, Policy p, ScenarioManager sm)
+        {
+            AccumObj = accumlator; P = p; SM = sm;
+
+            Init();
+
+            for (mNo = 1; mNo > 100; mNo++)
+            {
+                Logic1();
+                Logic2();
+                SetCF();
+            }
+        }
+
+        public void Init()
+        {
+
+            member1 = 0;
+            member2 = "";
+            member3 = 0.0;
+        }
+
+        private void Logic2()
+        {
+            member1 += 1;
+        }
+
+        private void Logic1()
+        {
+            member2 += "A";
+            member3 += 0.1;
+        }
+
+        public void SetCF()
+        {
+            AccumObj.SetCF(this, P, SM);
+        }
+    }
+
+    public class Model1 : ModelBase
+    {
+        public int member4;
+        public double member5;
+    }
+
+
+    public class Policy
+    {
+        string contNo;
+        double a;
+        double b;
+    }
+
+    public class ScenarioManager
+    {
+        string contNo;
+        double a;
+        double b;
+    }
+
+}
