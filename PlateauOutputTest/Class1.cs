3be1b9fd905e7df3202468fd1afa7e168f54af24
DESKTOP-3SKP3T7\KKH_GRAM <checknow@naver.com>
2020-03-03 21:53:27 +0900

    플라토아웃풋테스트 진행중

--- PlateauOutputTest/Class1.cs
+++ PlateauOutputTest/Class1.cs
@@ -10,8 +10,8 @@ namespace PlateauOutputTest
     {   
         ICFTable GetCF(IModel model, Policy p, ScenarioManager sm);
 
-        string TableName { get; }
-        void SetTableName(string tn);
+        //string TableName { get; }
+        //void SetTableName(string tn);
 
         void AddUp(ICFTable cf);
     }
@@ -22,8 +22,8 @@ namespace PlateauOutputTest
         public string Member2;
         public double Member3;
 
-        private string _tableName = "N/A";
-        public string TableName { get => _tableName; }
+        //private string _tableName = "N/A";
+        //public string TableName { get => _tableName; }
 
         public ICFTable GetCF(IModel model, Policy p, ScenarioManager sm)
         {
@@ -40,40 +40,103 @@ namespace PlateauOutputTest
             return cf;
         }
 
+        public void AddUp(CFTable1 cf)
+        { 
+            CFTable1 modelFrom = cf as CFTable1;
+            Member1 = modelFrom.Member1;
+            Member2 = modelFrom.Member2;
+            Member3 = modelFrom.Member3;          
+        }
+
         public void AddUp(ICFTable cf)
         {
-            if (cf.GetType() == typeof(CFTable1))
-            {
-                CFTable1 modelFrom = cf as CFTable1;
-                Member1 = modelFrom.Member1;
-                Member2 = modelFrom.Member2;
-                Member3 = modelFrom.Member3;
-            }
+            Member1 = 0;
+            Member2 = "0";
+            Member3 = 0.0;
         }
 
-        public void SetTableName(string tn)
-        {
-            _tableName = tn;
-        }
+        //public void SetTableName(string tn)
+        //{
+        //    _tableName = tn;
+        //}
     }
 
+    public enum GroupingType
+    { BySG = 0, ByScen = 1, BySGByScen = 2, ByPol = 3, ByMth = 4}
+
+    //public class GroupingDataLayout
+    //{
+    //    public GroupingType GroupingT;
+    //    public ICFTable[] GroupedData;
+    //    public Dictionary<string, int> GroupNameToNo;
+
+    //    public void Init(GroupingType gt, ) 
+
+    //    public void SetCF()
+    //    {
+
+    //    }
+    //}
+
+    // 그루핑 데이터, 단건에 대한 정보
     public class Accumlator
     {
         // 베이스가 되는 타입
         public Type CFTableType { get; private set; }
-        
-        // 그루핑된 데이터 저장소 : Dic<subGroup, ICFTable>
-        Dictionary<string, ICFTable> GroupedData;
+        public ICFTable Cf { get; private set; }
 
-        public void Init(ICFTable cfT)
+        public bool GroupsBySG { get; private set; } = true;
+        public bool GroupsByScen { get; private set; } = true;
+        public bool GroupsByPol { get; private set; } = false;
+        public bool GroupsByMth { get; private set; } = false;
+
+        // 그루핑된 데이터 저장소
+        ICFTable[] DtBySG;
+        ICFTable[] DtByScen;
+        ICFTable[] DtByPol;
+        ICFTable[] DtByMth;
+
+        Dictionary<string, int> SgToNo;
+
+        public void Init(ICFTable cfT, List<string> sgList = null)
         {
-            CFTableType = cfT.GetType();
+            if (GroupsBySG)
+            {
+                DtBySG = new ICFTable[sgList.Count];
+                for (int i = 0; i < sgList.Count; i++)
+                {
+                    DtByScen[i] = (ICFTable)Activator.CreateInstance(CFTableType);
+                }
+            }
+
+            if (GroupsByScen)
+            {
+                DtByScen = new ICFTable[100];
+                for (int i = 0; i < 100; i++)
+                {
+                    DtByScen[i] = (ICFTable)Activator.CreateInstance(CFTableType);
+                }
+
+            }
         }
 
-        public void SetCF(IModel model, Policy p, ScenarioManager sM)
+        public void SetCF(IModel model, Policy p, ScenarioManager sm)
         {
+            if (GroupsBySG)
+            {
+                int no = SgToNo[p.SubGroup];
+                DtBySG[no].AddUp(Cf.GetCF(model, p, sm));
+            }
 
-            throw new NotImplementedException();
+            if (GroupsByScen)
+            {
+                int no = SgToNo[p.SubGroup];
+                DtBySG[no].AddUp(Cf.GetCF(model, p, sm));
+            }
+
+            if (GroupsByPol) { }
+
+            if (GroupsByMth) { }            
         }
     }
 }
